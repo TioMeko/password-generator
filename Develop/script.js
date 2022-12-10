b1bc60667bbdd87c929b1940559a5f14b6aa0afa
NineTwoProgramming <61386052+NineTwoProgramming@users.noreply.github.com>
2022-12-10 16:21:54 -0500

    Updated JS generatePassword function

--- Develop/script.js
+++ Develop/script.js
@@ -1,26 +1,57 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var lowerCase = "abcdefghijklmnopqrstuvwxyz".split("");
+var upperCase = "abcdefghijklmnopqrstuvwxyz".toUpperCase().split("");
+var numbers = "1234567890".split("");
+var specialCharacters = " !\"#$%&'()*+,-./:;<=>?@[]^_`{|}~".split("");
 
 function generatePassword() {
-  var passLength = '';
+  //Empty Arrays to hold the data
+  var placeholder = [];
+  var generatedPassword = [];
+
+  //Asks user for length then checks conditional before continuing
   var passLength = prompt('How long do you want your password to be? (Between 8 and 128)');
-  console.log(`THIS IS A RENEWED ${passLength}`);
-  
-  if (passLength >= 8 && passLength <=128){
-    console.log(`Before going to writePassword: ${passLength}`);
-    return passLength;
-  }else {
-    alert("ERROR: Please choose a number between 8 and 128!");
-    console.log(`This is a false statement log ${passLength}`);
-    generatePassword();
+
+  if (passLength < 8 || passLength >128){
+    alert("ERROR: Please click `Generate Password` again and choose a number between 8 and 128!");
+    return '';
   };
 
+  //Asks user to confirm choices and uses conditionals to add the values to the blank placeholder array.
+  var lowerCaseChoice = confirm("Do you want lower case letters?");
+  var upperCaseChoice = confirm("Do you want upper case letters?");
+  var numbersChoice = confirm("You want any numbers?");
+  var specialCharactersChoice = confirm("Do you want special characters?");
+
+  if (lowerCaseChoice){
+    placeholder = placeholder.concat(lowerCase);
+  }
+
+  if (upperCaseChoice){
+    placeholder = placeholder.concat(upperCase);
+  }
+
+  if (numbersChoice){
+    placeholder = placeholder.concat(numbers);
+  }
+
+  if (specialCharactersChoice){
+    placeholder = placeholder.concat(specialCharacters);
+  }
+
+  //Runs a loop to add random combinations for the generatedPassword array
+  for (var i = 0; i < passLength; i++){
+    generatedPassword.push(placeholder[Math.floor(Math.random() * placeholder.length)]);
+  }
+
+  //Joins the generatedPassword array into a single string seperated by no space and returns the value
+  return generatedPassword.join('');
 }
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  console.log(`THIS IS THE REAL VALUE: ${password}`);
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
