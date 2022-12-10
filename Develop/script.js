bb08e502fa23b354132cdd42eac2e80f9537dad4
NineTwoProgramming <61386052+NineTwoProgramming@users.noreply.github.com>
2022-12-10 14:28:00 -0500

    updated JS

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-<<<<<<< HEAD
 function generatePassword() {
   var passLength = '';
   var passLength = prompt('How long do you want your password to be? (Between 8 and 128)');
@@ -22,11 +21,6 @@ function generatePassword() {
 function writePassword() {
   var password = generatePassword();
   console.log(`THIS IS THE REAL VALUE: ${password}`);
-=======
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
->>>>>>> fd6a6e9d0a00c16297f2f44cc9b4ac51dd3d0acf
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
@@ -35,7 +29,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-<<<<<<< HEAD
-
-=======
->>>>>>> fd6a6e9d0a00c16297f2f44cc9b4ac51dd3d0acf
