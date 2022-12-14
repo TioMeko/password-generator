d84987e381f21508df9d1516bfc379f3175bdc5d
NineTwoProgramming <61386052+NineTwoProgramming@users.noreply.github.com>
2022-12-14 11:50:22 -0500

    Added comments and updated README.md

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,30 +1,36 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
 var lowerCase = "abcdefghijklmnopqrstuvwxyz".split("");
 var upperCase = "abcdefghijklmnopqrstuvwxyz".toUpperCase().split("");
 var numbers = "1234567890".split("");
 var specialCharacters = " !\"#$%&'()*+,-./:;<=>?@[]^_`{|}~".split("");
 
+// Function that generates the password based on the criteria
 function generatePassword() {
-  //Empty Arrays to hold the data
+  // Empty Arrays to hold the data
   var placeholder = [];
   var generatedPassword = [];
 
-  //Asks user for length then checks conditional before continuing
+  // Asks user for password length then checks conditional before continuing
   var passLength = prompt('How long do you want your password to be? (Between 8 and 128)');
 
-  if (passLength < 8 || passLength >128){
+  if (passLength < 8 || passLength > 128){
     alert("ERROR: Please click `Generate Password` again and choose a number between 8 and 128!");
-    return '';
+    return "";
   };
 
-  //Asks user to confirm choices and uses conditionals to add the values to the blank placeholder array.
+  // Asks user to confirm choices and uses conditionals to add the values to the blank placeholder array.
   var lowerCaseChoice = confirm("Do you want lower case letters?");
   var upperCaseChoice = confirm("Do you want upper case letters?");
   var numbersChoice = confirm("You want any numbers?");
   var specialCharactersChoice = confirm("Do you want special characters?");
 
+  if (!lowerCaseChoice && !upperCaseChoice && !numbersChoice && !specialCharactersChoice){
+    alert("You must choose at least 1 character type. Please start over!");
+    return "";
+  }
+
   if (lowerCaseChoice){
+
     placeholder = placeholder.concat(lowerCase);
   }
 
@@ -40,13 +46,13 @@ function generatePassword() {
     placeholder = placeholder.concat(specialCharacters);
   }
 
-  //Runs a loop to add random combinations for the generatedPassword array
+  // Runs a loop to add random combinations for the generatedPassword array
   for (var i = 0; i < passLength; i++){
     generatedPassword.push(placeholder[Math.floor(Math.random() * placeholder.length)]);
   }
 
-  //Joins the generatedPassword array into a single string seperated by no space and returns the value
-  return generatedPassword.join('');
+  // Joins the generatedPassword array into a single string seperated by no space and returns the value
+  return generatedPassword.join("");
 }
 
 // Write password to the #password input
@@ -54,7 +60,7 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  passwordText.innerHTML = password;
 
 }
 
